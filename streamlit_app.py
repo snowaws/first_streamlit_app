d3a8d98122d82e19e34044487756415643847200
snowaws <104317122+snowaws@users.noreply.github.com>
2022-04-28 13:32:40 -0500

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('🥑 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥑 Kale, Spinach & Ricket Smoothie')
-streamlit.text('🥑 Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Ricket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacodo Toast')
