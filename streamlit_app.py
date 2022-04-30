df1d540636aea7b7fd6df3a31dfb64f60e60374a
snowaws <104317122+snowaws@users.noreply.github.com>
2022-04-30 17:21:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,10 @@ fruits_selected=streamlit.multiselect("Let us pick some fruits:", list(my_fruit_
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+
+streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
+#streamlit.text(fruityvice_response)
+
+streamlit.text(fruityvice_response.json())
