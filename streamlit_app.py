d2442fddad9bccbd80888b828b54cd29fee1b4cf
snowaws <104317122+snowaws@users.noreply.github.com>
2022-04-30 17:26:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,6 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response)
 
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
